d67323615b09170a01224281ef3db38d02861d9b
Prasanna-0707 <pbogachandrapu@proliantdatallc.com>
2026-08-05 11:27:29 +0530

    Initial Vite project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <h1>Proliant Data Website</h1>
+    </BrowserRouter>
+  );
+}
+
+export default App;
