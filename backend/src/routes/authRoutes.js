494955108206b2e15825c887e20c2823d2b1845c
Aswini99999 <ajarugula@proliantdatallc.com>
2026-09-11 19:31:35 +0530

    modified backend

---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  login,
+  logout,
+} from "../controllers/authController.js";
+
+import authMiddleware from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/login", login);
+
+router.post("/logout", authMiddleware, logout);
+
+export default router;
